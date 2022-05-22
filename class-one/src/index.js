eaa28502c54e34eff2a2eaa9b740d46519268431
fazlur <rahmanfadalur@gmail.com>
2022-05-22 12:19:34 +0530

    Added Initial Code

---
+++ class-one/src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client';
+
+const main = ReactDOM.createRoot(document.getElementById('main'));
+
+main.render(
+  <div>
+    <h1>This React App</h1>
+    <p>This Another Para</p>
+    <h1>This is another Heading</h1>
+  </div>
+)
+
